906ab6d1312956d4ab4e1498167013bd79dd1f41
JongukYang <didwhddnr123@naver.com>
2023-01-21 23:23:31 +0900

    init

---
+++ GusessMyMBTI/src/main/java/com/hsstudy/GusessMyMBTI/GusessMyMbtiApplication.java
@@ -0,0 +1,13 @@
+package com.hsstudy.GusessMyMBTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GusessMyMbtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GusessMyMbtiApplication.class, args);
+	}
+
+}
---
+++ GusessMyMBTI/src/test/java/com/hsstudy/GusessMyMBTI/GusessMyMbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsstudy.GusessMyMBTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GusessMyMbtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
